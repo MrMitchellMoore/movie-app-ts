755163e25fa2724abdf5b19e7735d1eda5b250ee
Mitchell Moore <mmoorejr24@gmail.com>
2023-07-13 13:19:18 -0700

    Finished UI SignUp page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,66 @@
-export default function SignUp() {
-  return <div>page</div>;
-}
+import Image from "next/image";
+import netflixBg from "@/public/netflixBg.jpg";
+import Link from "next/link";
+
+const SignUp = () => {
+  return (
+    <>
+      <div className="w-full h-screen">
+        <Image
+          alt="Netflix BG"
+          src={netflixBg}
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="100vw"
+          className="hidden sm:block absolute w-full h-full object-cover"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+              <form className="w-full flex flex-col py-4">
+                <input
+                  className="p-4 my-3 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="email"
+                  autoComplete="email"
+                />
+                <input
+                  className="p-4 my-3 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="password"
+                  autoComplete="current-password"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Sign Up
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input type="checkbox" className="mr-2" />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-8">
+                  <span className="text-gray-600 px-2">
+                    Already subscribed to Netflix?
+                  </span>
+                  <Link
+                    className="bg-gray-900 rounded px-2 py-1 text-sm hover:bg-gray-600"
+                    href={"/login"}
+                  >
+                    Sign In
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
